37f46f9090fce1dfe004fe5b71a09e2453c638c7
dmsumon2020 <dmsumon2020@gmail.com>
2025-07-16 21:29:14 +0600

    added hero section type3

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -424,6 +424,8 @@ export interface ApiHeroSectionHeroSection extends Struct.CollectionTypeSchema {
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    image: Schema.Attribute.Media<'images' | 'files', true> &
+      Schema.Attribute.Required;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
